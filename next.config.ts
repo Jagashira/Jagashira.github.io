e58380049e811781bcb43e89d12750dd748eaa01
jagashira <pyprogjas.3104@gmail.com>
2025-05-19 13:57:01 +0900

    init deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
